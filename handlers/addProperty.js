c01dae0c2bec4467bf8235537f36d961e7adf4fa
Jonathan Abi <jonathanabi81@gmail.com>
2024-03-19 22:21:41 +0700

    fix addProperty

--- handlers/addProperty.js
+++ handlers/addProperty.js
@@ -1,9 +1,43 @@
 const dbConnect = require('.././dbConnection')
+const shortid = require('shortid');
+const length = 8;
 
 async function addProperty(req, res) {
+    const {
+        agent,
+        title,
+        desc,
+        type,
+        area,
+        price,
+        bedroom_count,
+        bathroom_count,
+        land_area,
+        garage,
+        floor_level,
+        listing_date,
+        approved_date,
+        status
+    } = req.body;
     try {
         const db = await dbConnect();
-        const result = await db.collection('property').insertOne(req.body);
+        const result = await db.collection('property').insertOne({
+            id : shortid.generate().substring(0, length),
+            agent,
+            title,
+            desc,
+            type,
+            area,
+            price,
+            bedroom_count,
+            bathroom_count,
+            land_area,
+            garage,
+            floor_level,
+            listing_date,
+            approved_date,
+            status
+        });
         res.status(201).json({ status: 201, message: 'Property baru berhasil ditambahkan' });
     } catch (error) {
         res.status(500).json({ error: error.message });
