d885afa5be33f26c5bfbfed86e85f7a6b928e285
koceoey <nicho2505.nc@gmail.com>
2024-03-20 21:38:42 +0700

    fix addProperty and authorization on properties function

--- Controller/handlers/propertyHandler.js
+++ Controller/handlers/propertyHandler.js
@@ -1,17 +1,17 @@
 const { connectToMongoDB } = require('../dbConnection');
-const { checkUserType } = require('./authenticationHandler');
+const { getTokenData, checkUserType } = require('./authenticationHandler');
+
 const shortid = require('shortid');
 const length = 8;
 
-async function addPropertyHandler(req, res) {
+async function addProperty(req, res) {
     const authHeader = req.headers['authorization'];
     if(!checkUserType(authHeader, 1)){
-        console.log("anjai agent");
         res.status(401).json({ status: 401, message: 'Error: Invalid credentials' });
+        return;
     }
-
+    
     const {
-        agent,
         title,
         desc,
         type,
@@ -21,35 +21,50 @@ async function addPropertyHandler(req, res) {
         bathroom_count,
         land_area,
         garage,
-        floor_level,
-        listing_date,
-        approved_date,
-        status
+        floor_level
     } = req.body;
+
+    const propertyData = {};
+
+    propertyData.id = shortid.generate().substring(0, length);
+
+    const { id } = getTokenData(authHeader);
+    agent = id;
+
+    // check required fields
+    if (agent === undefined || title === undefined || desc === undefined || type === undefined || area === undefined || price === undefined) {
+        res.status(400).json({ status: 400, message: 'Required fields are missing' });
+        return;
+    }
+
+    propertyData.agent = agent;
+    propertyData.title = title;
+    propertyData.desc = desc;
+    propertyData.type = type;
+    propertyData.area = type;
+    propertyData.price = price;
+
+    // check undefined entries
+    if (bedroom_count !== undefined) propertyData.bedroom_count = bedroom_count;
+    if (bathroom_count !== undefined) propertyData.bathroom_count = bathroom_count;
+    if (land_area !== undefined) propertyData.land_area = land_area;
+    if (garage !== undefined) propertyData.garage = garage;
+    if (floor_level !== undefined) propertyData.floor_level = floor_level;
+
+    propertyData.listing_date = new Date();
+    propertyData.approved_date = "";
+    propertyData.status = 0;
+
     try {
         const db = await connectToMongoDB();
-        const result = await db.collection('property').insertOne({
-            id : shortid.generate().substring(0, length),
-            agent,
-            title,
-            desc,
-            type,
-            area,
-            price,
-            bedroom_count,
-            bathroom_count,
-            land_area,
-            garage,
-            floor_level,
-            listing_date,
-            approved_date,
-            status
-        });
-        res.status(201).json({ status: 201, message: 'Property baru berhasil ditambahkan' });
+        const result = await db.collection('property').insertOne(propertyData);
+        console.log(propertyData);
+        res.status(201).json({ status: 201, message: 'Successfully add property listing request' });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 }
+
 
 async function getPropertyHandler(req, res) {
     const { type } = req.body;
@@ -67,6 +82,11 @@ async function getPropertyHandler(req, res) {
 }
 
 async function approvePropertyHandler(req, res) {
+    const authHeader = req.headers['authorization'];
+    if(!checkUserType(authHeader, 0)){
+        res.status(401).json({ status: 401, message: 'Error: Invalid credentials' });
+        return;
+    }
     const { id } = req.body;
     const currentDate = new Date(); 
     const date = ("0" + currentDate.getDate()).slice(-2);
