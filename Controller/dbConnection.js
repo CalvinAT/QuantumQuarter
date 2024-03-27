321324ab85f79abcdf2d5848d8572018ccbb8191
Nicholas Chrisandy <nicho2505.nc@gmail.com>
2024-03-27 22:29:29 +0700

    test

--- Controller/dbConnection.js
+++ Controller/dbConnection.js
@@ -3,43 +3,24 @@ const config = require('./config');
 const mysql = require('mysql2/promise');
 const sqlConfig = require('./sqlConfig');
 
-// Singleton class for managing database connections
-class DatabaseManager {
-    constructor() {
-        if (!DatabaseManager.instance) {
-            this.mongoClient = null;
-            this.mysqlPool = null;
-            DatabaseManager.instance = this;
-        }
-        return DatabaseManager.instance;
-    }
-
-    async connectToMongoDB() {
-        try {
-            if (!this.mongoClient) {
-                this.mongoClient = new MongoClient(config.mongodb_url);
-                await this.mongoClient.connect();
-            }
-            return this.mongoClient.db('quantum-quarters');
-        } catch (error) {
-            console.log('Error connecting to MongoDB: ', error);
-            throw error;
-        }
-    }
-
-    async connectToMySQL() {
-        try {
-            if (!this.mysqlPool) {
-                this.mysqlPool = mysql.createPool(sqlConfig);
-            }
-            return this.mysqlPool;
-        } catch (error) {
-            console.error('Error connecting to MySQL:', error);
-            throw error;
-        }
+async function connectToMongoDB() {
+    try {
+        const client = new MongoClient(config.mongodb_url);
+        await client.connect();
+        return db = client.db('quantum-quarters')
+    } catch (error) {
+        console.log('Error connecting to MongoDB: ', error)
     }
 }
 
-const databaseManager = new DatabaseManager();
+async function connectToMySQL() {
+    try {
+        pool = mysql.createPool(sqlConfig);
+    } catch (error) {
+        console.error('Error connecting to MySQL:', error);
+        throw error;
+    }
+    return pool;
+}
 
-module.exports = databaseManager;
+module.exports = {connectToMongoDB, connectToMySQL};
