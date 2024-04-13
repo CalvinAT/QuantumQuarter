4c42ed28654cbe63efde8726dea43505139be671
koceoey <nicho2505.nc@gmail.com>
2024-04-13 15:54:01 +0700

    update agent data and fix add employee

--- Controller/handlers/employeeHandler.js
+++ Controller/handlers/employeeHandler.js
@@ -1,5 +1,5 @@
 const { connectToMySQL } = require('../dbConnection');
-const { checkUserType } = require('./authenticationHandler');
+const { checkUserType, getTokenData } = require('./authenticationHandler');
 const bcrypt = require('bcrypt');
 const path = require('path');
 
@@ -20,50 +20,49 @@ async function addEmployee(req, res) {
         email,
         password,
         type,
-        branch_id,
-        phone_number,
+        branchId,
+        phoneNumber,
         whatsapp,
     } = req.body;
-
-    const profile = req.file;
-
-    const projectId = 'quantumquarters';
-    const keyFilename = path.resolve(__dirname, 'quantumquarters-storage.json');
-
-    const storage = new Storage({
-        projectId,
-        keyFilename
-    });
-
-    const bucketName = 'quantum-quarters-employee';
-    const uniqueFolderName = `${id}/`;
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
         const pool = await connectToMySQL();
 
-        const filePath = profile.path;
-        const fileName = profile.originalname;
-
-        if(filePath){
-            await storage.bucket(bucketName).upload(filePath, {
-                destination: `${uniqueFolderName}${fileName}`,
-                metadata: {
-                    contentType: profile.mimetype,
-                    defaultObjectAcl: 'publicRead',
-                },
-            })
-        } else {
-            console.log('File path error')
-        }
-        const profilePath = `https://storage.googleapis.com/${bucketName}/${uniqueFolderName}${fileName}`
         // Insert employee
         await pool.query('INSERT INTO employee (id, name, address, gender, email, password, type, currently_login) VALUES (?, ?, ?, ?, ?, ?, ?, 0);', [id, name, address, gender, email, hashedPassword, type]);
 
         // Insert agent if type is 1
         if (type === '1') {
-            await pool.query('INSERT INTO agent (id, branch_id, phone_number, whatsapp, profile_path) VALUES (?, ?, ?, ?, ?);', [id, branch_id, phone_number, whatsapp, profilePath]);
+            const profile = req.file;
+
+            const projectId = 'quantumquarters';
+            const keyFilename = path.resolve(__dirname, 'quantumquarters-storage.json');
+
+            const storage = new Storage({
+                projectId,
+                keyFilename
+            });
+
+            const bucketName = 'quantum-quarters-employee';
+            const uniqueFolderName = `${id}/`;
+            const filePath = profile.path;
+            const fileName = profile.originalname;
+
+            if(filePath){
+                await storage.bucket(bucketName).upload(filePath, {
+                    destination: `${uniqueFolderName}${fileName}`,
+                    metadata: {
+                        contentType: profile.mimetype,
+                        defaultObjectAcl: 'publicRead',
+                    },
+                })
+            } else {
+                console.log('File path error')
+            }
+            const profilePath = `https://storage.googleapis.com/${bucketName}/${uniqueFolderName}${fileName}`
+            await pool.query('INSERT INTO agent (id, branch_id, phone_number, whatsapp, profile_path) VALUES (?, ?, ?, ?, ?);', [id, branchId, phoneNumber, whatsapp, profilePath]);
         }
 
         // Send success response
@@ -74,4 +73,42 @@ async function addEmployee(req, res) {
     }
 }
 
-module.exports = addEmployee;
+async function updateAgentProfile(req, res){
+    const {
+        phoneNumber,
+        whatsapp,
+    } = req.body;
+
+    try {
+        const authHeader = req.headers['authorization'];
+        if(authHeader === undefined || !checkUserType(authHeader, 1)){
+            throw new Error("Invalid Credentials.");
+        }
+
+        const { id } = getTokenData(authHeader);
+        let query = 'UPDATE agent SET ';
+
+        // check phone number input
+        if(phoneNumber !== undefined){
+            query += `phone_number = '${phoneNumber}'`;
+        }
+
+        // check whatsapp input
+        if(whatsapp !== undefined){
+            if(phoneNumber !== undefined){
+                query+= ', ';
+            }
+            query += `whatsapp = '${whatsapp}'`;
+        }
+
+        query += ` WHERE id = '${id}'`;
+        const pool = await connectToMySQL();
+        await pool.query(query);
+
+        res.status(201).json({ status: 201, message: 'Agent data successfully updated' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+module.exports = {addEmployee, updateAgentProfile};
