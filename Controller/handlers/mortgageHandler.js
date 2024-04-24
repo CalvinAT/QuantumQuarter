07dc6de92bda433d49b8da83adba8bde57fbdab2
Nicholas Chrisandy <nicho2505.nc@gmail.com>
2024-04-24 12:00:11 +0700

    property detail done and facade

--- Controller/handlers/mortgageHandler.js
+++ Controller/handlers/mortgageHandler.js
@@ -23,21 +23,10 @@ async function countMortgageSimulation(req, res){
         mortgagePayment = mortgagePayment.toLocaleString('en-US', { maximumFractionDigits: 0 });
         res.status(200).json({ status: 200, mortgagePayment });
         // add mortgage log
-        //addMortgageLog(req, price, downPayment, loanAmount, loanTerm, interest, mortgagePayment);
         await MortgageLogFacade.addMortgageLog(req, price, downPayment, loanAmount, loanTerm, interest, mortgagePayment);
     } catch (error) {
         // Handle errors
         res.status(400).json({ error: error.message });
-    }
-}
-
-async function addMortgageLog(req, price, downPayment, loanAmount, loanTerm, interest, mortgagePayment){
-    const pool = await connectToMySQL();
-    try {
-        const ipAddress = req.ip;
-        await pool.query('INSERT INTO kpr (ip_address, price, down_payment, loan, loan_time, interest, monthly_installment) VALUES (?,?,?,?,?,?,?);', [ipAddress, price, downPayment, loanAmount, loanTerm, interest, mortgagePayment]);
-    } catch (error) {
-        console.error('Error during add log :', error);
     }
 }
 
