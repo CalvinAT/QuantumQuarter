64d8ddc0ebaee51a75c47603e662d6d0a50533a2
Nicholas Chrisandy <nicho2505.nc@gmail.com>
2024-04-25 00:19:28 +0700

    last fix

--- Controller/routes.js
+++ Controller/routes.js
@@ -9,25 +9,26 @@ const countMortgageSimulation = require('./handlers/mortgageHandler');
 const {getFeedback, addFeedback} = require('./handlers/feedbackHandler')
 const upload = multer({ dest: 'uploads/' });
 
+// property handlers
 router.post('/property', upload.array('images',5), addPropertyHandler);
 router.post('/getproperty', upload.none(), getPropertyHandler);
 router.put('/property', upload.none(), setStatusPropertyHandler);
 router.post('/deleteproperty', upload.none(), setStatusPropertyHandler);
-
 router.post('/getdetailedproperty', upload.none(), getDetailedPropertyHandler);
 
+// employee handlers
 router.post('/employee', upload.single('image'), addEmployee);
-
 router.post('/getemployee', upload.none(), getProfile);
-
 router.put('/agent', upload.none(), updateAgentProfile);
 
+// login handlers
 router.post('/login', upload.none(), login)
-
 router.get('/logout', upload.none(), logout)
 
+// mortgage handlers
 router.post('/mortgage', upload.none(), countMortgageSimulation)
 
+// feedback handlers
 router.post('/feedback', upload.none(), addFeedback)
 router.get('/feedback', upload.none(), getFeedback)
 
